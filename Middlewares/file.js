ccb945809ec485dcce628fa584bfa533644a463f
era <era.user.ivan@gmail.com>
2022-05-04 08:50:05 +0300

    Убрал закомментированный код в методе multipleReadFile

--- Middlewares/file.js
+++ Middlewares/file.js
@@ -27,18 +27,6 @@ export const multipleReadFile = async (files) => {
     let fileUrl = [];
     for (let i = 0; i < files.length; i++) {
         fileUrl.push(await readFile(files[i]))
-
-        // const {createReadStream, filename} = await files[i].file;
-        // const stream = createReadStream();
-        // let {ext, name} = parse(filename);
-        // name = `single${Math.floor((Math.random() * 10000) + 1)}`;
-        // let url = join(__dirname, `../Upload/${name}-${Date.now()}${ext}`);
-        // const imageStream = await createWriteStream(url)
-        // await stream.pipe(imageStream);
-        // const baseUrl = process.env.BASE_URL
-        // const port = process.env.PORT
-        // url = `${baseUrl}${port}${url.split('Upload')[1]}`;
-        // fileUrl.push(url);
     }
     return fileUrl
 }
